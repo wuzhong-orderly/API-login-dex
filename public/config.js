08f9a2a7461cfe840a8355f11b64cd9d304280fd
Wuzhong <wuzhong@orderly.network>
2026-06-09 11:08:46 +0800

    remove with/deposit related buttons

--- public/config.js
+++ public/config.js
@@ -18,7 +18,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_DESCRIPTION": "Built for traders who demand full custody of their assets without sacrificing execution speed, liquidity depth, or a professional trading interface.",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
-  "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets,Leaderboard,Campaigns",
+  "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets",
   "VITE_CUSTOM_MENUS": "",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
