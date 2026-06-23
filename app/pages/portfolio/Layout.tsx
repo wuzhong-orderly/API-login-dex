39fa5c5d46ddb95e5eb95e9ac64dbfe1eb640fae
Wuzhong <wuzhong@orderly.network>
2026-06-23 22:25:04 +0800

    Filter out api keys from portfolio

--- app/pages/portfolio/Layout.tsx
+++ app/pages/portfolio/Layout.tsx
@@ -1,5 +1,8 @@
 import { Outlet, useLocation } from "react-router-dom";
-import { PortfolioLayoutWidget } from "@orderly.network/portfolio";
+import {
+  PortfolioLayoutWidget,
+  usePortfolioLayoutScript,
+} from "@orderly.network/portfolio";
 import { useOrderlyConfig } from "@/utils/config";
 import { useNav } from "@/hooks/useNav";
 
@@ -9,9 +12,12 @@ export default function PortfolioLayout() {
 
   const { onRouteChange } = useNav();
   const config = useOrderlyConfig();
+  const { items } = usePortfolioLayoutScript({ current: pathname });
+  const filteredItems = items.filter((item) => item.href !== "/portfolio/apiKey");
 
   return (
     <PortfolioLayoutWidget
+      items={filteredItems}
       footerProps={config.scaffold.footerProps}
       mainNavProps={{
         ...config.scaffold.mainNavProps,
